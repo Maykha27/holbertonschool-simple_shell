2f6fc1369f6998bc7e4e68f65491b151f7199b26
Rbrogniez <105486378+Rbrogniez@users.noreply.github.com>
2022-08-02 15:28:05 +0200

    Add files via upload

---
+++ shell.c
@@ -0,0 +1,54 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main()
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *token;
+    int i;
+    int child = 0;
+
+    char **args = malloc(sizeof(char *) * 1024);
+
+
+    while ((nread = getline(&line, &len, stdin)) != -1)
+    {
+
+        i = 0;
+        token = strtok(line, " \n");
+        child = fork();
+
+         while (token != NULL)
+        {
+            args[i] = token;
+            token = strtok(NULL, " \n");
+            i++;
+        }
+
+        if (child == 0)
+        if (execve(args[0], args, NULL) == -1)
+        {
+            perror("Error:");
+        }
+		wait(NULL);
+
+    }
+
+    free(line);
+    free(args);
+
+    exit(EXIT_SUCCESS);
+}
