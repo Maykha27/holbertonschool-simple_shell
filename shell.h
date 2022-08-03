ec4d58d001a50874392be078bac56406325952f3
Rbrogniez <105486378+Rbrogniez@users.noreply.github.com>
2022-08-03 16:11:00 +0200

    add shell.h

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef DF9CF737_E144_484B_A91B_8321CD3ECDD1
+#define DF9CF737_E144_484B_A91B_8321CD3ECDD1
+#ifndef HEADER
+#define HEADER
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <signal.h>
+
+#endif
+
+
+#endif /* DF9CF737_E144_484B_A91B_8321CD3ECDD1 */
